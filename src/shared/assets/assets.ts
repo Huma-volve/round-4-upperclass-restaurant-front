c7b1aa15d499f3bbb031f2cf18c7fdc16cbbeb7d
AliiiHassann <alihassan71010@gmail.com>
2025-07-09 19:43:10 +0300

    all tasks with the fonts

--- src/shared/assets/assets.ts
+++ src/shared/assets/assets.ts
@@ -2,10 +2,42 @@ import card1 from "./card1.svg";
 import card2 from "./card2.svg";
 import card3 from "./card3.svg";
 import card4 from "./card4.svg";
+import frontPage from "./front-page.webp";
+import menuPage from "./menu.webp";
+import menu1 from "./menu-1.jpg";
+import menu2 from "./menu-2.jpg";
+import menu3 from "./menu-3.jpg";
+import menu4 from "./menu-4.jpg";
+import menu5 from "./menu-5.jpg";
+import menu6 from "./menu-6.jpg";
+import menu7 from "./menu-7.png";
+import menu8 from "./menu-8.jpg";
+import menu9 from "./menu-9.jpg";
+import menu10 from "./menu-10.jpg";
+import menu11 from "./menu-11.webp";
+import menu12 from "./menu-12.jpg";
+import menu13 from "./menu-13.webp";
+import menu14 from "./menu-14.webp";
 
 export const assets = {
   card1,
   card2,
   card3,
   card4,
+  frontPage,
+  menuPage,
+  menu1,
+  menu2,
+  menu3,
+  menu4,
+  menu5,
+  menu6,
+  menu7,
+  menu8,
+  menu9,
+  menu10,
+  menu11,
+  menu12,
+  menu13,
+  menu14,
 };
