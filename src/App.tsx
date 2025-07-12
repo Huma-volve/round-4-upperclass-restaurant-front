698213e3df6ca19d0cb21f477f7623002001222e
radwa okaily <74832631+radwaokaily@users.noreply.github.com>
2025-07-13 02:15:26 +0300

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+import { Route, Routes } from "react-router-dom";
 import { assets } from "./shared/assets/assets";
+import Navbar from "./shared/layout/Navbar";
+import Layout from "./shared/layout/Layout";
 
 function App() {
   return (
-    <div className="container mx-auto px-4 2xl:px-20">
-      <img src={assets.card2} alt="notFound" />
+    <>
+    <div className="container mx-auto px-4 2xl:px-20 bg-amber-600 h-50 flex items-center">
+    <Routes>
+      <Route path="/" element={<Layout/>}/>
+        
+    </Routes>
+    
+    
+      
+       {/* <Navbar/> */}
     </div>
+    </>
   );
 }
 
